89ee6ab63e38069e0a36601151fbae5af082ea13
GitHub Action <action@github.com>
2022-08-10 10:46:08 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("smartparkingtemplate")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
